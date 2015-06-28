1f9c2e606dfceedad3da2ff9a0262ef60a644741
unknown <lkutvolg@HUB-LKUTVOLG-W7.mgc.mentorg.com>
2015-06-28 07:43:02 +0200

    first commit

---
+++ src/com/lory/bible/parts/BooksPart.java
@@ -0,0 +1,22 @@
+ 
+package com.lory.bible.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class BooksPart {
+	@Inject
+	public BooksPart() {
+		//TODO Your code here
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		//TODO Your code here
+	}
+	
+	
+	
+	
+}
