a928df5c34b2ce220ccd6978144d3dbddfe739fc
Kutvolgyi Lorand <lorand.kutvolgyi@gmail.com>
2015-07-05 17:19:37 +0200

    project removed

--- src/main/java/com/lory/bible/util/BibleUtil.java
+++
@@ -1,8 +0,0 @@
-package com.lory.bible.util;
-
-public class BibleUtil {
-    public final static String[] oldTestamenBooksName = { "Gen", "Exo", "Lev", "Num", "Deu", "Jos", "Judg", "Rut",
-            "1Sa", "2Sa", "1Ki", "1Ki", "1Ch", "2Ch", "Ezr", "Neh", "Est", "Job", "Psa", "Pro", "Ecc", "Song", "Isa",
-            "Jer", "Lam", "Eze", "Dan", "Hos", "Joe", "Amo", "Obd", "Jon", "Mic", "Nah", "Hab", "Zep", "Hag", "Zec",
-            "Mal" };
-}
