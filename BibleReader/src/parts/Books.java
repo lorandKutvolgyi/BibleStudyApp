7b1815fce594dd7edfd84a1bc30dd729b0f7b0dd
Kutvolgyi Lorand <lorand.kutvolgyi@gmail.com>
2015-07-05 22:38:47 +0200

    new project structure

---
+++ BibleReader/src/parts/Books.java
@@ -0,0 +1,34 @@
+package parts;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CLabel;
+import org.eclipse.swt.widgets.Composite;
+
+public class Books {
+    @Inject
+    public Books() {
+        // TODO Your code here
+    }
+
+    @PostConstruct
+    public void postConstruct(Composite parent) {
+        createTree(parent);
+        parent.setLayout(null);
+
+        CLabel lblOldTestament = new CLabel(parent, SWT.NONE);
+        lblOldTestament.setBounds(10, 10, 84, 21);
+        lblOldTestament.setText("Old Testament");
+
+        CLabel lblGenesis = new CLabel(parent, SWT.NONE);
+        int baseYPos = 39;
+        lblGenesis.setBounds(20, baseYPos, 61, 21);
+        lblGenesis.setText("Genesis");
+
+    }
+
+    private void createTree(Composite parent) {
+    }
+}
