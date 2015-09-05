761b20b480053608b69cf888d72ed1aca6f7304c
lorandKutvolgyi <lorand.kutvolgyi@gmail.com>
2015-09-05 19:03:07 +0200

    liquibase introduced

---
+++ BibleReader/resources/runLiquibase.bat
@@ -0,0 +1,2 @@
+cd %~dp0
+java -jar liquibase.jar --driver=com.mysql.jdbc.Driver --classpath=mysql-connector-java-5.1.6.jar --changeLogFile=db.changelog.xml --url="jdbc:mysql://localhost:3306/Bible?useUnicode=true&characterEncoding=UTF-8" --username=root update
