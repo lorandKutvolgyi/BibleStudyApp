1b21a79b9873854cff9baafc118695a396d7d518
lorandKutvolgyi <lorand.kutvolgyi@gmail.com>
2015-09-05 19:08:33 +0200

    i18n introduced

---
+++ BibleReader/src/i18n/Messages.java
@@ -0,0 +1,78 @@
+package i18n;
+
+import org.eclipse.e4.tools.services.Message;
+
+@Message(contributorURI = "platform:/plugin/BibleReader/resource")
+public class Messages {
+    public String GENESIS;
+    public String EXODUS;
+    public String LEVITICUS;
+    public String NUMBERS;
+    public String DEUTERONOMY;
+    public String JOSHUA;
+    public String JUDGES;
+    public String RUTH;
+    public String I_SAMUEL;
+    public String II_SAMUEL;
+    public String I_KINGS;
+    public String II_KINGS;
+    public String I_CHRONICLES;
+    public String II_CHRONICLES;
+    public String EZRA;
+    public String NEHEMIAH;
+    public String ESTHER;
+    public String JOB;
+    public String PSALMS;
+    public String PROVERBS;
+    public String ECCLESIASTES;
+    public String SONG_OF_SOLOMON;
+    public String ISAIAH;
+    public String JEREMIAH;
+    public String LAMENTATIONS;
+    public String EZEKIEL;
+    public String DANIEL;
+    public String HOSEA;
+    public String JOEL;
+    public String AMOS;
+    public String OBADIAH;
+    public String JONAH;
+    public String MICAH;
+    public String NAHUM;
+    public String HABAKKUK;
+    public String ZEPHANIAH;
+    public String HAGGAI;
+    public String ZECHARIAH;
+    public String MALACHI;
+
+    public String MATTHEW;
+    public String MARK;
+    public String LUKE;
+    public String JOHN;
+    public String ACTS;
+    public String ROMANS;
+    public String I_CORINTHIANS;
+    public String II_CORINTHIANS;
+    public String GALATIANS;
+    public String EPHESIANS;
+    public String PHILIPPIANS;
+    public String COLOSSIANS;
+    public String I_THESSALONIANS;
+    public String II_THESSALONIANS;
+    public String I_TIMOTHY;
+    public String II_TIMOTHY;
+    public String TITUS;
+    public String PHILEMON;
+    public String HEBREWS;
+    public String JAMES;
+    public String I_PETER;
+    public String II_PETER;
+    public String I_JOHN;
+    public String II_JOHN;
+    public String III_JOHN;
+    public String JUDE;
+    public String REVELATION_OF_JOHN;
+
+    public String OldTestament;
+    public String NewTestament;
+
+}
