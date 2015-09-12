8baa64cb0a6b50c3d813fbaba1c8ed002f29bbe4
lorandKutvolgyi <lorand.kutvolgyi@gmail.com>
2015-09-12 09:47:06 +0200

    jar files removed to lib folder

--- BibleReader/runLiquibase.bat
+++ BibleReader/runLiquibase.bat
@@ -1,2 +1,2 @@
 cd %~dp0
-java -jar resources\liquibase.jar --driver=com.mysql.jdbc.Driver --classpath=resources\mysql-connector-java-5.1.6.jar --changeLogFile=resources\db.changelog.xml --url="jdbc:mysql://localhost:3306/Bible?useUnicode=true&characterEncoding=UTF-8" --username=root update
+java -jar lib\liquibase.jar --driver=com.mysql.jdbc.Driver --classpath=lib\mysql-connector-java-5.1.6.jar --changeLogFile=resources\db.changelog.xml --url="jdbc:mysql://localhost:3306/Bible?useUnicode=true&characterEncoding=UTF-8" --username=root update
