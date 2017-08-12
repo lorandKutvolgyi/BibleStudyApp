fcf944dc88f62339654fc5d20cbff67f066da757
Kútvölgyi Lóránd <lorand.kutvolgyi@gmail.com>
2017-08-12 17:33:09 +0200

    new parts created

---
+++ Bible/BibleReader/src/com/lory/biblereader/parts/mapstack/mappart/MapPart.java
@@ -0,0 +1,22 @@
+ 
+package com.lory.biblereader.parts.mapstack.mappart;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class MapPart {
+	@Inject
+	public MapPart() {
+		
+	}
+	
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+	
+	
+	
+	
+}
